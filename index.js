70f2e79fa8adcbaabd5f200553eeddef7955a88c
Casmir5 <agbocasmir2@gmail.com>
2024-03-21 08:11:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElementsLeft = document.querySelectorAll(".hide-element-left");
+const hiddenElementsRight = document.querySelectorAll(".hide-element-right");
+
+hiddenElementsLeft.forEach((el) => observer.observe(el));
+hiddenElementsRight.forEach((el) => observer.observe(el));
+
+const siderbar = document.querySelector(".sidebar");
+const openSidebar = document.querySelector(".open-sidebar");
+const closeSidebar = document.querySelector(".close-sidebar");
+
+openSidebar.addEventListener("click", function () {
+  siderbar.classList.remove("translate-x-full");
+  siderbar.classList.add("translate-x-0");
+});
+closeSidebar.addEventListener("click", function () {
+  siderbar.classList.add("translate-x-full");
+  siderbar.classList.remove("translate-x-0");
+});
